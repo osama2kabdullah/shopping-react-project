5883e94cfe30ce7f54fefcb15ab378a50dacf98e
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-09 20:20:13 +0600

    active link created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../App.css';
 import logo from '../images/Logo.svg'
+import CustomLink from './CustomLink';
 
 
 
@@ -10,10 +10,10 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div className='nav-links'>
-                <Link to="/">Shop</Link>
-                <Link to="/order">Order Review</Link>
-                <Link to="/inventory">Manage Inventory</Link>
-                <Link to="/login">Login</Link>
+                <CustomLink to="/">Shop</CustomLink>
+                <CustomLink to="/order">Order Review</CustomLink>
+                <CustomLink to="/inventory">Manage Inventory</CustomLink>
+                <CustomLink to="/login">Login</CustomLink>
             </div>
         </nav>
     );
